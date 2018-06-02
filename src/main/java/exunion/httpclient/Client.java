a5dc915cef1834b85eee3be27d0229987de7fd91
robothy <robothyluo@gmail.com>
2018-06-02 22:16:10 +0800

    添加POST重载方法，使其能够发送表单数据。

--- src/main/java/exunion/httpclient/Client.java
+++ src/main/java/exunion/httpclient/Client.java
@@ -1,22 +1,28 @@
 package exunion.httpclient;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.ParseException;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -107,7 +113,33 @@ public class Client {
 	public String post(String uri, Map<String, String> header){
 		HttpRequestBase post = new HttpPost(uri);
 		return this.httpBaseOperate(post, header);
-	} 
+	}
+	
+	/**
+	 * 向服务器发送一个带参数的post请求
+	 * @param uri 请求资源的地址
+	 * @param header 一个包含头部信息的 Map，其中 key 放名称，value放值
+	 * @param form 待提交的实体
+	 * @return 服务器返回的文本结果
+	 */
+	public String post(String uri, Map<String, String> header, Map<String, String> form){
+		HttpPost post = new HttpPost(uri);
+		if(form != null){
+			List<NameValuePair> parameters = new ArrayList<>();
+			for(Entry< String, String> entry : form.entrySet()){
+				NameValuePair nvp = new BasicNameValuePair(entry.getKey(), entry.getValue());
+				parameters.add(nvp);
+			}
+			HttpEntity entity = null;
+			try {
+				entity = new UrlEncodedFormEntity(parameters, "UTF-8");
+			} catch (UnsupportedEncodingException e) {
+				LOGGER.error("构建 POST 请求实体时出现异常。", e);
+			}
+			post.setEntity(entity);
+		}
+		return this.httpBaseOperate(post, header);
+	}
 	
 	private String httpBaseOperate(HttpRequestBase requestBase, Map<String, String> header){
 		HttpResponse response = null;
