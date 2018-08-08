a572578c642be1c1f746cba361a8203148c4a1ab
robothy <robothyluo@gmail.com>
2018-08-08 22:08:46 +0800

    改了改

--- src/main/java/exunion/util/EncryptionTools.java
+++ src/main/java/exunion/util/EncryptionTools.java
@@ -1,13 +1,9 @@
 package exunion.util;
 
-import java.io.UnsupportedEncodingException;
-import java.security.InvalidKeyException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-
+import java.io.*;
+import java.security.*;
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
-
 import org.apache.commons.codec.binary.Hex;
 
 public class EncryptionTools {
@@ -19,11 +15,11 @@ public class EncryptionTools {
 	 * @return the cipher
 	 */
 	public static String HmacSHA256(String secret, String data){
-		return HmanSHA(secret, data, "HmacSHA256");
+		return HmacSHA(secret, data, "HmacSHA256");
 	}
 	
 	public static byte[] HmacSHA256Hex(String secret, String data){
-		return HmanSHAHex(secret, data, "HmacSHA256");
+		return HmacSHAHex(secret, data, "HmacSHA256");
 	}
 	
 	/**
@@ -33,11 +29,19 @@ public class EncryptionTools {
 	 * @return the cipher
 	 */
 	public static String HmacSHA512(String secret, String data){
-		return HmanSHA(secret, data, "HmacSHA512");
+		return HmacSHA(secret, data, "HmacSHA512");
 	}
-	
+
+	public static String HmacSHA1(String secret, String data){
+	    return HmacSHA(secret, data, "HmacSHA1");
+    }
+
+    public static byte[] HmacSHA1Hex(String secret, String data){
+	    return HmacSHAHex(secret, data, "HmacSHA1");
+    }
+
 	public static String HmacMd5(String secret, String data){
-		return HmanSHA(secret, data, "HmacMD5");
+		return HmacSHA(secret, data, "HmacMD5");
 	}
 	
 	public static String SHA256(String data){
@@ -94,11 +98,11 @@ public class EncryptionTools {
 		return encdeStr;
 	}
 	
-	private static String HmanSHA(String secret, String data, String algorithm){
-		return Hex.encodeHexString(HmanSHAHex(secret, data, algorithm));
+	private static String HmacSHA(String secret, String data, String algorithm){
+		return Hex.encodeHexString(HmacSHAHex(secret, data, algorithm));
 	}
 	
-	private static byte[] HmanSHAHex(String secret, String data, String algorithm){
+	private static byte[] HmacSHAHex(String secret, String data, String algorithm){
 		
 		byte[] cipher = null; 
 		
@@ -126,18 +130,17 @@ public class EncryptionTools {
 		return cipher;
 	
 	}
-	
-	
-	
+
 	public static void main(String[] args){
-		//System.out.println(HmanSHA("6186ec9223b8f8e3fe28b5f5c831427ed99950a6", "accesskey=6d8f62fd-3086-46e3-a0ba-c66a929c24e2&method=getAccountInfo", "HmacMD5"));
+
+		//System.out.println(HmacSHA("6186ec9223b8f8e3fe28b5f5c831427ed99950a6", "accesskey=6d8f62fd-3086-46e3-a0ba-c66a929c24e2&method=getAccountInfo", "HmacMD5"));
 		//System.out.println(SHA1("48939bbc-8d49-402b-b731-adadf2ea9628"));
-		System.out.println(HmacMd5("wZYHnNS396a4F5OVviUHG3dBgGK1zPTyFRwnRYsJIYy40CerZKgsN4ECrd8KHDX6", "api_key=48e5e01ca50cc6ac722b6502d3cd938e&nonce=559120&timestamp=1520347559wZYHnNS396a4F5OVviUHG3dBgGK1zPTyFRwnRYsJIYy40CerZKgsN4ECrd8KHDX6"));
+		/*System.out.println(HmacMd5("wZYHnNS396a4F5OVviUHG3dBgGK1zPTyFRwnRYsJIYy40CerZKgsN4ECrd8KHDX6", "api_key=48e5e01ca50cc6ac722b6502d3cd938e&nonce=559120&timestamp=1520347559wZYHnNS396a4F5OVviUHG3dBgGK1zPTyFRwnRYsJIYy40CerZKgsN4ECrd8KHDX6"));
 		System.out.println(HmacMd5("wZYHnNS396a4F5OVviUHG3dBgGK1zPTyFRwnRYsJIYy40CerZKgsN4ECrd8KHDX6", "api_key=48e5e01ca50cc6ac722b6502d3cd938e&nonce=559120&timestamp=1520347559"));
 		System.out.println(HmacMd5("wZYHnNS396a4F5OVviUHG3dBgGK1zPTyFRwnRYsJIYy40CerZKgsN4ECrd8KHDX6", "api_key=48e5e01ca50cc6ac722b6502d3cd938e&nonce=559120&timestamp=1520347559wZYHnNS396a4F5OVviUHG3dBgGK1zPTyFRwnRYsJIYy40CerZKgsN4ECrd8KHDX6"));
 		System.out.println(HmacMd5("wZYHnNS396a4F5OVviUHG3dBgGK1zPTyFRwnRYsJIYy40CerZKgsN4ECrd8KHDX6", "api_key=48e5e01ca50cc6ac722b6502d3cd938e&nonce=559120&timestamp=1520347559"));
 		System.out.println(HmacMd5("wZYHnNS396a4F5OVviUHG3dBgGK1zPTyFRwnRYsJIYy40CerZKgsN4ECrd8KHDX6", "api_key=48e5e01ca50cc6ac722b6502d3cd938e&nonce=559120&timestamp=1520347559wZYHnNS396a4F5OVviUHG3dBgGK1zPTyFRwnRYsJIYy40CerZKgsN4ECrd8KHDX6"));
-		System.out.println(HmacMd5("wZYHnNS396a4F5OVviUHG3dBgGK1zPTyFRwnRYsJIYy40CerZKgsN4ECrd8KHDX6", "api_key=48e5e01ca50cc6ac722b6502d3cd938e&nonce=559120&timestamp=1520347559"));
+		System.out.println(HmacMd5("wZYHnNS396a4F5OVviUHG3dBgGK1zPTyFRwnRYsJIYy40CerZKgsN4ECrd8KHDX6", "api_key=48e5e01ca50cc6ac722b6502d3cd938e&nonce=559120&timestamp=1520347559"));*/
 	}
 	
 	
