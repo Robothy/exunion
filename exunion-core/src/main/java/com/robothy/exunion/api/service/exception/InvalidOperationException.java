b1e311549bf1febf2948b4c5818443c0aac91e75
Luo <Robothy@users.noreply.github.com>
2021-02-12 11:47:57 +0800

    code refactor (#11)

--- exunion-core/src/main/java/com/robothy/exunion/api/service/exception/InvalidOperationException.java
+++
@@ -1,9 +0,0 @@
-package com.robothy.exunion.api.service.exception;
-
-public class InvalidOperationException extends RuntimeException {
-
-    public InvalidOperationException(String message) {
-        super(message);
-    }
-
-}
