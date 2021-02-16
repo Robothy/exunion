59f6255ac4504a061e53d657af8a95629dc6131c
Luo <Robothy@users.noreply.github.com>
2021-02-16 21:04:34 +0800

    update spot trading interface (#18)
    * config github maven repository
    
    * add sign util
    
    * update spot trading API

--- exunion-rest/src/main/java/com/robothy/exunion/rest/spot/SpotTradingService.java
+++ exunion-rest/src/main/java/com/robothy/exunion/rest/spot/SpotTradingService.java
@@ -5,18 +5,23 @@ import com.robothy.exunion.core.trade.spot.Order;
 import com.robothy.exunion.rest.ExchangeService;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Spot trading service.
  */
 public interface SpotTradingService extends ExchangeService {
 
-    Order buy(Order order) throws ExchangeException, IOException;
+    Order place(Order order) throws ExchangeException, IOException;
 
-    Order sell(Order order) throws ExchangeException, IOException;
+    List<Order> place(List<Order> orders) throws ExchangeException, IOException;
 
     Order cancel(Order order) throws ExchangeException, IOException;
 
+    List<Order> cancel(List<Order> orders) throws ExchangeException, IOException;
+
     Order query(Order order) throws ExchangeException, IOException;
+
+    List<Order> query(List<Order> orders) throws ExchangeException, IOException;
 
 }
