a060fac6c7cd8adfc591e29a511cce1676511d8f
Robothy <robothyluo@gmail.com>
2021-02-16 21:02:27 +0800

    update spot trading API

--- exunion-service-huobi/src/main/java/com/robothy/exunion/huobi/trade/spot/HuobiSpotService.java
+++ exunion-service-huobi/src/main/java/com/robothy/exunion/huobi/trade/spot/HuobiSpotService.java
@@ -1,58 +1,47 @@
-package com.robothy.exunion.rest.spot;
+package com.robothy.exunion.huobi.trade.spot;
 
 import com.robothy.exunion.core.exception.ExchangeException;
 import com.robothy.exunion.core.meta.SupportedExchange;
 import com.robothy.exunion.core.trade.spot.Order;
+import com.robothy.exunion.rest.spot.SpotTradingService;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Objects;
 
-/**
- * Proxy of SpotTradingService. Some common around logic could be written in this proxy class.
- */
-public class SpotTradingServiceProxy implements SpotTradingService {
+public class HuobiSpotService implements SpotTradingService {
 
-    private final SpotTradingService instance;
-
-    public SpotTradingServiceProxy(SpotTradingService instance) {
-        Objects.requireNonNull(instance, "The spot trading service instance shouldn't be null");
-        this.instance = instance;
+    @Override
+    public SupportedExchange exchange() {
+        return null;
     }
-
 
     @Override
     public Order place(Order order) throws ExchangeException, IOException {
-        return this.instance.place(order);
+        return null;
     }
 
     @Override
     public List<Order> place(List<Order> orders) throws ExchangeException, IOException {
-        return this.instance.place(orders);
+        return null;
     }
 
     @Override
     public Order cancel(Order order) throws ExchangeException, IOException {
-        return this.instance.cancel(order);
+        return null;
     }
 
     @Override
     public List<Order> cancel(List<Order> orders) throws ExchangeException, IOException {
-        return this.instance.cancel(orders);
+        return null;
     }
 
     @Override
     public Order query(Order order) throws ExchangeException, IOException {
-        return this.instance.query(order);
+        return null;
     }
 
     @Override
     public List<Order> query(List<Order> orders) throws ExchangeException, IOException {
-        return this.instance.query(orders);
-    }
-
-    @Override
-    public SupportedExchange exchange() {
-        return this.instance.exchange();
+        return null;
     }
 }
