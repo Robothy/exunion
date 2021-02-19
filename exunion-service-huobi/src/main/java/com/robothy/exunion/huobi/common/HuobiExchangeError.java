70de257a08653bc65d112def8e6caa488ec3f37d
Robothy <robothyluo@gmail.com>
2021-02-19 21:12:43 +0800

    add HuobiExchangeError to create ExchangeError instance

---
+++ exunion-service-huobi/src/main/java/com/robothy/exunion/huobi/common/HuobiExchangeError.java
@@ -0,0 +1,27 @@
+package com.robothy.exunion.huobi.common;
+
+import com.robothy.exunion.core.meta.SupportedExchange;
+import com.robothy.exunion.core.trade.ExchangeErrorMessage;
+
+public class HuobiExchangeError {
+
+    public static ExchangeErrorMessage of(String code, String msg){
+        return new ExchangeErrorMessage() {
+            @Override
+            public String code() {
+                return code;
+            }
+
+            @Override
+            public String message() {
+                return msg;
+            }
+
+            @Override
+            public SupportedExchange exchange() {
+                return SupportedExchange.HUOBI;
+            }
+        };
+    }
+
+}
