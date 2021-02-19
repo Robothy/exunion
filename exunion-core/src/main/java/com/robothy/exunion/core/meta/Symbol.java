5bb999c764fe6b125429c5121d6ea4f96116788b
Robothy <robothyluo@gmail.com>
2021-02-19 16:58:42 +0800

    override equals() and hashCode()

--- exunion-core/src/main/java/com/robothy/exunion/core/meta/Symbol.java
+++ exunion-core/src/main/java/com/robothy/exunion/core/meta/Symbol.java
@@ -1,5 +1,7 @@
 package com.robothy.exunion.core.meta;
 
+import java.util.Objects;
+
 public class Symbol {
 
     private Currency base;
@@ -27,8 +29,28 @@ public class Symbol {
         this.quote = quote;
     }
 
+    public static Symbol of(Currency base, Currency quote){
+        return new Symbol(base, quote);
+    }
+
+    public static Symbol of(String base, String quote){
+        return new Symbol(Currency.valueOf(base), Currency.valueOf(quote));
+    }
+
     @Override
     public String toString() {
         return String.format("%s/%s", base, quote);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.base, this.quote);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(!(obj instanceof Symbol)) return false;
+        Symbol o = (Symbol) obj;
+        return o == this || (o.base == this.base && o.quote == this.quote);
     }
 }
