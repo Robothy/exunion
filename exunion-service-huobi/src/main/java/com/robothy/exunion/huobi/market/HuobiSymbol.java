84e28bc3164d30d82e1d04fc9996f3ae9c28ac10
Robothy <robothyluo@gmail.com>
2021-02-19 17:31:36 +0800

    update HuobiSymbol

--- exunion-service-huobi/src/main/java/com/robothy/exunion/huobi/market/HuobiSymbol.java
+++ exunion-service-huobi/src/main/java/com/robothy/exunion/huobi/market/HuobiSymbol.java
@@ -9,17 +9,38 @@ public class HuobiSymbol extends Symbol {
         super(base, quote);
     }
 
+    /**
+     * huobi symbol doesn't contains a separator between base currency and quote currency.
+     * So we need to split it manually.
+     * @param huobiSymbol huobi symbol in string format.
+     */
+    public HuobiSymbol(String huobiSymbol){
+        super(null,null);
+        int quoteLen = 2; // HT
+        if (huobiSymbol.endsWith("eth") || huobiSymbol.endsWith("btc")) {
+            quoteLen = 3;
+        } else if (huobiSymbol.endsWith("usdt") || huobiSymbol.endsWith("husd") || huobiSymbol.endsWith("alts")) {
+            quoteLen = 4;
+        }
+        super.setBase(Currency.valueOf(huobiSymbol.substring(0, huobiSymbol.length() - quoteLen).toUpperCase()));
+        super.setQuote(Currency.valueOf(huobiSymbol.substring(huobiSymbol.length() - quoteLen).toUpperCase()));
+    }
+
     @Override
     public String toString() {
         return (getBase().toString() + getQuote().toString()).toLowerCase();
     }
 
-    public static Symbol of(Currency base, Currency quote){
+    public static Symbol of(Currency base, Currency quote) {
         return new HuobiSymbol(base, quote);
     }
 
-    public static Symbol of(Symbol symbol){
-        return symbol == null ? null : of(symbol.getBase(), symbol.getQuote());
+    public static Symbol of(Symbol symbol) {
+        return of(symbol.getBase(), symbol.getQuote());
+    }
+
+    public static Symbol of(String symbol){
+        return new HuobiSymbol(symbol);
     }
 
 }
