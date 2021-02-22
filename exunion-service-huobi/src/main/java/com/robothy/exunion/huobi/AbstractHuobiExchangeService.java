3fc4660b1fb0913c24fc04b46d0a7b1b24d606ca
Robothy <robothyluo@gmail.com>
2021-02-22 11:29:28 +0800

    handle string response requirement.

--- exunion-service-huobi/src/main/java/com/robothy/exunion/huobi/AbstractHuobiExchangeService.java
+++ exunion-service-huobi/src/main/java/com/robothy/exunion/huobi/AbstractHuobiExchangeService.java
@@ -2,6 +2,7 @@ package com.robothy.exunion.huobi;
 
 import com.google.api.client.http.GenericUrl;
 import com.google.api.client.http.HttpMethods;
+import com.google.api.client.http.HttpResponse;
 import com.google.api.client.http.json.JsonHttpContent;
 import com.robothy.exunion.core.meta.Exchange;
 import com.robothy.exunion.huobi.meta.Huobi;
@@ -32,12 +33,10 @@ public abstract class AbstractHuobiExchangeService extends AbstractExchangeServi
     protected <T> T post(String path, Object body, Class<T> responseType) throws IOException {
         String url = path.startsWith("http") ? path : options.getApiServer() + path;
         logRequest(HttpMethods.POST, url, body);
-        T response = options.getHttpRequestFactory()
+        HttpResponse httpResponse = options.getHttpRequestFactory()
                 .buildPostRequest(new GenericUrl(url), new JsonHttpContent(options.getJsonFactory(), body))
-                .execute()
-                .parseAs(responseType);
-        logResponse(response);
-        return response;
+                .execute();
+        return parseResult(httpResponse, responseType);
     }
 
     /**
@@ -51,12 +50,21 @@ public abstract class AbstractHuobiExchangeService extends AbstractExchangeServi
     protected <T> T get(String path, Class<T> responseType) throws IOException {
         String url = path.startsWith("http") ? path : options.getApiServer() + path;
         logRequest(HttpMethods.GET, url);
-        T response = options.getHttpRequestFactory()
+        HttpResponse httpResponse = options.getHttpRequestFactory()
                 .buildGetRequest(new GenericUrl(url))
-                .execute()
-                .parseAs(responseType);
-        logResponse(response);
-        return response;
+                .execute();
+        return parseResult(httpResponse, responseType);
+    }
+
+    private <T> T parseResult(HttpResponse httpResponse, Class<T> responseType) throws IOException {
+        T result;
+        if(responseType == String.class){
+            result = responseType.cast(httpResponse.parseAsString());
+        }else {
+            result = httpResponse.parseAs(responseType);
+        }
+        logResponse(result);
+        return result;
     }
 
     private void logRequest(String method, String url) throws IOException {
@@ -65,9 +73,11 @@ public abstract class AbstractHuobiExchangeService extends AbstractExchangeServi
 
     private void logRequest(String method, String url, Object body) throws IOException {
         if(LOGGER.isDebugEnabled()){
-            StringBuilder text = new StringBuilder().append("\n")
-                    .append(exchange()).append("\n")
-                    .append(method).append("\n");
+            StringBuilder text = new StringBuilder()
+                    .append("Request -> ").append("\n")
+                    .append(exchange().id()).append("\n")
+                    .append(method).append("\n")
+                    .append(url).append("\r");
             if(null != body) text.append(options.getJsonFactory().toPrettyString(body));
             LOGGER.debug(text.toString());
         }
@@ -75,9 +85,14 @@ public abstract class AbstractHuobiExchangeService extends AbstractExchangeServi
 
     private void logResponse(Object response) throws IOException {
         if(LOGGER.isDebugEnabled()){
-            StringBuilder text = new StringBuilder().append("\n")
-                    .append(exchange()).append("\n")
-                    .append(options.getJsonFactory().toPrettyString(response));
+            StringBuilder text = new StringBuilder()
+                    .append("Response -> ").append("\n")
+                    .append(exchange().id()).append("\n");
+            if(response instanceof String){
+                text.append(response);
+            }else{
+                text.append(options.getJsonFactory().toPrettyString(response));
+            }
             LOGGER.debug(text.toString());
         }
     }
