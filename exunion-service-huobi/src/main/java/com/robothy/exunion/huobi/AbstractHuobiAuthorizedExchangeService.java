609786d438195a347ecec048e8b2d430af8fab78
Robothy <robothyluo@gmail.com>
2021-02-22 10:09:48 +0800

    add common methods

--- exunion-service-huobi/src/main/java/com/robothy/exunion/huobi/AbstractHuobiAuthorizedExchangeService.java
+++ exunion-service-huobi/src/main/java/com/robothy/exunion/huobi/AbstractHuobiAuthorizedExchangeService.java
@@ -1,8 +1,10 @@
 package com.robothy.exunion.huobi;
 
+import com.google.api.client.http.HttpMethods;
 import com.robothy.exunion.huobi.util.HuobiSignUtil;
 import com.robothy.exunion.rest.spi.Options;
 
+import java.io.IOException;
 import java.util.Map;
 import java.util.Objects;
 
@@ -13,6 +15,43 @@ public abstract class AbstractHuobiAuthorizedExchangeService extends AbstractHuo
         super.init(options);
         Objects.requireNonNull(options.getToken().getApiKey(), "The access key is required to visited Huobi authorized service.");
         Objects.requireNonNull(options.getToken().getApiSecret(), "The secret key is required to visited Huobi authorized service.");
+    }
+
+    /**
+     * POST data with signature to huobi API server.
+     * @param path the API entrypoint without url parameters.
+     * @param params the URL parameters to sign and build the full request URL.
+     * @param body the request body.
+     * @param responseType the response class.
+     * @param <T> the response data type.
+     * @return the response body.
+     * @throws IOException when network error occurs.
+     */
+    protected <T> T postWithSign(String path, Map<String, Object> params, Object body, Class<T> responseType) throws IOException {
+        String url = signedUrl(HttpMethods.POST, path, params);
+        return super.post(url, body, responseType);
+    }
+
+    /**
+     * GET data with signature.
+     * @param path the API entrypoint with url parameter.
+     * @param params the URL parameters to sign and build the request URL.
+     * @param responseType the response class.
+     * @param <T> the response type.
+     * @return the response.
+     * @throws IOException when network error occurs.
+     */
+    protected <T> T getWithSign(String path, Map<String, Object> params, Class<T> responseType) throws IOException {
+        String url = signedUrl(HttpMethods.GET, path, params);
+        return super.get(url, responseType);
+    }
+
+    protected <T> T postWithSign(String path, Object body, Class<T> responseType) throws IOException {
+        return this.postWithSign(path, null, body, responseType);
+    }
+
+    protected <T> T getWithSign(String path, Class<T> responseType) throws IOException {
+        return this.getWithSign(path, null, responseType);
     }
 
     protected String signedUrl(String method, String path, Map<String, Object> params){
--- exunion-service-huobi/src/main/java/com/robothy/exunion/huobi/AbstractHuobiExchangeService.java
+++ exunion-service-huobi/src/main/java/com/robothy/exunion/huobi/AbstractHuobiExchangeService.java
@@ -1,14 +1,85 @@
 package com.robothy.exunion.huobi;
 
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpMethods;
+import com.google.api.client.http.json.JsonHttpContent;
 import com.robothy.exunion.core.meta.Exchange;
 import com.robothy.exunion.huobi.meta.Huobi;
 import com.robothy.exunion.rest.AbstractExchangeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 public abstract class AbstractHuobiExchangeService extends AbstractExchangeService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AbstractHuobiExchangeService.class);
 
     @Override
     public Exchange exchange() {
         return Huobi.SINGLETON;
+    }
+
+    /**
+     * Huobi RestAPI POST request without query parameters.
+     * @param path the API entrypoint or full URL (starts with 'http').
+     * @param body the request body
+     * @param responseType the target response type that the response body will be parsed to.
+     * @param <T> the response type.
+     * @return the response body.
+     * @throws IOException when network error occurs.
+     */
+    protected <T> T post(String path, Object body, Class<T> responseType) throws IOException {
+        String url = path.startsWith("http") ? path : options.getApiServer() + path;
+        logRequest(HttpMethods.POST, url, body);
+        T response = options.getHttpRequestFactory()
+                .buildPostRequest(new GenericUrl(url), new JsonHttpContent(options.getJsonFactory(), body))
+                .execute()
+                .parseAs(responseType);
+        logResponse(response);
+        return response;
+    }
+
+    /**
+     * Huobi API GET operation.
+     * @param path the API entrypoint or full URL (starts with 'http').
+     * @param responseType the target java type of the response.
+     * @param <T> the response type.
+     * @return parsed response.
+     * @throws IOException network error occurs.
+     */
+    protected <T> T get(String path, Class<T> responseType) throws IOException {
+        String url = path.startsWith("http") ? path : options.getApiServer() + path;
+        logRequest(HttpMethods.GET, url);
+        T response = options.getHttpRequestFactory()
+                .buildGetRequest(new GenericUrl(url))
+                .execute()
+                .parseAs(responseType);
+        logResponse(response);
+        return response;
+    }
+
+    private void logRequest(String method, String url) throws IOException {
+        logRequest(method, url, null);
+    }
+
+    private void logRequest(String method, String url, Object body) throws IOException {
+        if(LOGGER.isDebugEnabled()){
+            StringBuilder text = new StringBuilder().append("\n")
+                    .append(exchange()).append("\n")
+                    .append(method).append("\n");
+            if(null != body) text.append(options.getJsonFactory().toPrettyString(body));
+            LOGGER.debug(text.toString());
+        }
+    }
+
+    private void logResponse(Object response) throws IOException {
+        if(LOGGER.isDebugEnabled()){
+            StringBuilder text = new StringBuilder().append("\n")
+                    .append(exchange()).append("\n")
+                    .append(options.getJsonFactory().toPrettyString(response));
+            LOGGER.debug(text.toString());
+        }
     }
 
 }
