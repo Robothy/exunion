d0e6e3035f973cb617cf3771adca6319435006d3
Robothy <robothyluo@gmail.com>
2021-02-22 10:40:11 +0800

    add builder

--- exunion-core/src/main/java/com/robothy/exunion/core/trade/spot/SpotOrder.java
+++ exunion-core/src/main/java/com/robothy/exunion/core/trade/spot/SpotOrder.java
@@ -3,6 +3,7 @@ package com.robothy.exunion.core.trade.spot;
 import com.robothy.exunion.core.meta.Symbol;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.Map;
 
 public class SpotOrder {
@@ -96,4 +97,59 @@ public class SpotOrder {
         LIMIT, MARKET
     }
 
+    public static class Builder {
+
+        private final SpotOrder spotOrder;
+
+        private Builder(){
+            spotOrder = new SpotOrder();
+        }
+
+        public static Builder create(){
+            return new Builder();
+        }
+
+        public Builder side(Side side){
+            spotOrder.side = side;
+            return this;
+        }
+
+        public Builder type(Type type){
+            spotOrder.type = type;
+            return this;
+        }
+
+        public Builder symbol(Symbol symbol){
+            spotOrder.symbol = symbol;
+            return this;
+        }
+
+        public Builder quantity(BigDecimal quantity){
+            spotOrder.quantity = quantity;
+            return this;
+        }
+
+        public Builder price(BigDecimal price){
+            spotOrder.price = price;
+            return this;
+        }
+
+        /**
+         * Add an extra property.
+         * @param key the property key.
+         * @param value the property value.
+         *
+         */
+        public Builder extra(String key, Object value){
+            if(this.spotOrder.extraInfo == null){
+                this.spotOrder.extraInfo = new HashMap<>();
+            }
+            spotOrder.extraInfo.put(key, value);
+            return this;
+        }
+
+        public SpotOrder build(){
+            return this.spotOrder;
+        }
+    }
 }
